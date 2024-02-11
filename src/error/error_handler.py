d464ef1e616f47a7d010404c6ccc81803dbcb25a
prg-maker <df3296709@gmail.com>
2024-02-11 16:22:24 -0300

    fead: Implement Validator

--- src/error/error_handler.py
+++ src/error/error_handler.py
@@ -1,6 +1,23 @@
 from src.views.http_types.http_response import HttpResponse
+from .error_types.http_unprossable_entity import HttpUnprocessableEntityError
 
 def handle_erros(error:Exception) -> HttpResponse:
+
+    if isinstance(error, HttpUnprocessableEntityError):
+
+
+        # enviar para um log
+        # enviar um email de notificaçao
+        return  HttpResponse(
+            status_code=error.status_code,
+            body = {
+                "erros": [{
+                    "title":error.name,
+                    "detail": error.message
+                }]
+            }
+        )
+
     return HttpResponse(
         status_code=500,
         body={
